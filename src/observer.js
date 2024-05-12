0d0ccb9fb6c0868107c1783ed6ea248313c5d6cc
Oxy8 <34687508+Oxy8@users.noreply.github.com>
2024-05-12 01:09:35 -0300

    Tabela turmas funcionando e criados botoes grades.

--- src/observer.js
+++ src/observer.js
@@ -10,13 +10,13 @@ var observer = new MutationObserver(
             totalRemoved += record.removedNodes.length;
         }
 
-        if (totalRemoved == 5) {
+        if (totalRemoved == 5) { // o que exatamente significa esse caso aqui? Precisa remover seletor mesmo?
             //removeSeletorTurmas();
             console.log("Removed:" + totalRemoved)
         } 
 
         if (totalAdded == 5) {
-            //removeBotoesOriginais();
+            removeBotoesOriginais();
             insereSeletorTurmas();
             console.log("Added:" + totalAdded)
         }
@@ -51,59 +51,30 @@ function insereSeletorTurmas() {
 
     let tabela = divAtividades.getElementsByTagName("table")[0];
 
-    let rowBotaoAtualiza = tabela.insertRow(4);
-    let cellBotaoAtualiza = rowBotaoAtualiza.insertCell(0);
-    cellBotaoAtualiza.colSpan = 1;
-    cellBotaoAtualiza.width = "100%";
-    // width is deprecated
-    // and <td> not getting to full <tr> width, idk why.
-    // fix later.
+    let rowBotaoSelecionaTurmas = tabela.insertRow(4);
+    let cellBotaoSelecionaTurmas = rowBotaoSelecionaTurmas.insertCell(0);
+    cellBotaoSelecionaTurmas.colSpan = 3;
 
     const botaoSelecionaTurmas = document.createElement('input');
     botaoSelecionaTurmas.className = "button";
     botaoSelecionaTurmas.type = "button";
     botaoSelecionaTurmas.value = "Mostrar turmas diponíveis";
-    
-    //botaoSelecionaTurmas.addEventListener("click", LogaTurmasDisciplinasEscolhidas);
-    // Chamada para LogaTurmasDisciplinasEscolhidas()
 
-    botaoSelecionaTurmas.addEventListener("click", LogaTurmasDisciplinasEscolhidas2);
+    botaoSelecionaTurmas.addEventListener("click", MostraTabelaTurmasDisponiveis);
     
     const paragBotaoSelecionaTurmas = document.createElement('p');
     paragBotaoSelecionaTurmas.className = "paragBotoes";
     paragBotaoSelecionaTurmas.appendChild(botaoSelecionaTurmas);
-    cellBotaoAtualiza.appendChild(paragBotaoSelecionaTurmas);
+    cellBotaoSelecionaTurmas.appendChild(paragBotaoSelecionaTurmas);
 
 
-    let rowSeletorTurmas = tabela.insertRow(5);
-    let cellSeletorTurmas = rowSeletorTurmas.insertCell(0);
 
-    getSeletorTurmas();
+
 }
 
-function getSeletorTurmas() {
-    // deve mostrar as turmas disponíveis junto com informações a respeito do professor
-    // e horário e te permite 
 
-    // usar funções já existentes
-
-
-    // Usar 
-}
-
-// Procedimento:
-// Obtem todas turmas das cadeiras usando função abaixo
-// pede pro usuário selecionar aquelas turmas que ele quiser.
-// fazer Parsing de Horário-Período-Local
-// para aquelas turmas que o usuário selecionou, mostrar todas combinações possíveis.
-// 
-// Um possível seletor de prioridade fica para mais tarde, aqui basta mostrar todas possíveis.
-
-
-// todo right now:
-// parsing de Horário-Período-Local para valores em uma tabela.
-
-function LogaTurmasDisciplinasEscolhidas2() {
+/*
+function MostraTabelaTurmasDisponiveis() {
     
     var curriculoSelectVal = document.getElementById( "Curriculo" ).value;
     const [CodCur, CodHab] = curriculoSelectVal.split("/").map(item => item.trim());
@@ -123,28 +94,146 @@ function LogaTurmasDisciplinasEscolhidas2() {
             Sem: Semestre
         }, data => {
             
-            var tableElement = pegaTabelaTurmas(data);
+            var tableElement = pegaTabelaTurma(data);
             
-            var rowLength = tableElement.rows.length;
-            for (var i = 1; i < rowLength; i++) {
+            for (var i = 0; i < tableElement.rows.length; i++) {
                 
                 var celulaHorarios = tableElement.rows[i].cells[4];
                 for (var child of celulaHorarios.children) {
-                    console.log(child);
+                    
+                    var toBeParsed = child.innerHTML.split("<br>", 1)[0];
+                    
+                    const toBeParsedSplit= toBeParsed.trim().split(" ");
+                    if (toBeParsedSplit.length == 5) {
+                        const [Dia, HorarioInicio, , , NumeroPeriodos] = toBeParsedSplit;
+                        
+                        console.log(Dia);
+                        console.log(HorarioInicio);
+                        console.log(NumeroPeriodos);
+
+                        // vou ter uma tabela 7x16, onde serão marcados os períodos.
+                        // cada cadeira vai ter a sua própria tabela.
+
+                        // Depois para montar horários, criar função verifica conflitos horários.
+                    }
                 }
             }
         });
     }
+}
+*/
+
+
+function MostraTabelaTurmasDisponiveis() {
+    // aqui eu tenho que limpar possíveis adições prévias de
+    // MostraTabelaTurmasDisponiveis() && InsereBotoesMostraGrades()
+
+
+    var tabelaSelecaoTurmas = ObtemTabelaTurmasDisponiveis();
+
+    let tabela = divAtividades.getElementsByTagName("table")[0];
+
+    let rowBotaoAtualiza = tabela.insertRow(5);
+    let cellBotaoAtualiza = rowBotaoAtualiza.insertCell(0);
+
+    cellBotaoAtualiza.appendChild(tabelaSelecaoTurmas);
+
+    //=========================================================================
+
+    InsereBotoesMostraGrades();
+    // Remover botões originais, inserir novos aqui
+}
+
+function InsereBotoesMostraGrades() {
+
+    let tabela = divAtividades.getElementsByTagName("table")[0];
+
+    let rowBotoesMostraGrades = tabela.insertRow(6);
+    let cellBotoesGrades = rowBotoesMostraGrades.insertCell(0);
+    cellBotoesGrades.colSpan = 3;
+
+    const botaoGrades = document.createElement('input');
+    botaoGrades.className = "button";
+    botaoGrades.type = "button";
+    botaoGrades.value = "Mostrar Grades de Horários";
+
+    const botaoGradeUnica = document.createElement('input');
+    botaoGradeUnica.className = "button";
+    botaoGradeUnica.type = "button";
+    botaoGradeUnica.value = "Mostrar Grade Única de Horários";
+
+    botaoGrades.addEventListener("click", MostraTabelaTurmasDisponiveis);
+    botaoGradeUnica.addEventListener("click", MostraTabelaTurmasDisponiveis);
+    
+    const paragBotoesGrades = document.createElement('p');
+    paragBotoesGrades.className = "paragBotoes";
+    paragBotoesGrades.appendChild(botaoGrades);
+    paragBotoesGrades.appendChild(botaoGradeUnica);
+    cellBotoesGrades.appendChild(paragBotoesGrades);
 
 }
 
-function pegaTabelaTurmas(htmlString) {
+function ObtemTabelaTurmasDisponiveis() {
+    
+    var tabelaSelecaoTurmas = document.createElement('div');
+    tabelaSelecaoTurmas.id = "TabelaSelecaoTurmas";
+
+    //=========================================================================
+    var curriculoSelectVal = document.getElementById( "Curriculo" ).value;
+    const [CodCur, CodHab] = curriculoSelectVal.split("/").map(item => item.trim());
+
+    var cadeirasSelecionadas = Array.from(document.getElementById( "AtivEnsinoSelecionadas" ).options);
+    var codigosCadeirasSelecionadas = cadeirasSelecionadas.map(item => {
+        return item.value.split(",")[1].trim();
+    });
+
+    var Semestre = document.getElementById( "PeriodoLetivo" ).value;
+    //-------------------------------------------------------------------------
+
+    for (var CodAtiv of codigosCadeirasSelecionadas) {
+        $.get('/PortalEnsino/GraduacaoAluno/view/HorarioAtividade.php', {
+            CodAtiv: CodAtiv,
+            CodHab: CodHab,
+            CodCur: CodCur,
+            Sem: Semestre
+        }, data => {
+            
+            var tableElement = pegaTabelaTurma(data);
+
+            for (var row of tableElement.rows) {
+
+                var newRow = row.cloneNode(true);
+
+                var celulaBotao = newRow.insertCell(0);
+                var checkbox = document.createElement('input');
+                checkbox.type = 'checkbox';             
+                celulaBotao.appendChild(checkbox);
+
+                tabelaSelecaoTurmas.appendChild(newRow);
+            }
+        });
+    }
+    
+    return tabelaSelecaoTurmas;
+}
+
+
+function pegaTabelaTurma(htmlString) {
     var div = document.createElement('div');
     div.innerHTML = htmlString.trim();
   
-    // Change this to div.childNodes to support multiple top-level nodes.
-    return div.getElementsByTagName('table')[0];
-  }
+    var tableElement = div.getElementsByTagName('table')[0];
+    tableElement.deleteRow(0);
+
+    return tableElement;
+}
+
+// Pega tabela me retorna tabela com info de 1 turma.
+// Criar botão que quando clicado, mostra a tabela
+// de seleção de turmas, e ao mesmo tempo,
+// armazena em algum lugar essa tabela para uso futuro?
+
+
 
 // Obter a tabela me ajuda de alguma maneira?
 // é  mais fácil fazer parsing de "Terça 13:30 - 15:10 (2)" ou do código da cadeira?
